7305a5f26eef75b88581f801f0cb837b381c5d4a
Daniil <dan9m@ya.ru>
2017-06-19 16:22:01 +0300

    responsive fix

--- src/js/main.js
+++ src/js/main.js
@@ -5,36 +5,15 @@
         accessToken:'2262603067.1677ed0.6b8de62624684410b4470cf656a05561',
         get: 'user',
         userId: '2262603067',
-        resolution: 'thumbnail',
+        resolution: 'low_resolution',
         limit:20
     });
-
-    var wall = new Freewall("#instafeed");
-    wall.reset({
-        selector: 'a',
-        animate: true,
-        cellW: 150,
-        cellH: 150,
-        onResize: function() {
-            wall.fitWidth();
-        }
-    });
-
     $(document).ready(function () {
         feed.run();
         // for scroll bar appear;
         setTimeout(function () {
             feed.next();
         }, 1000);
-        setTimeout(function () {
-            feed.next();
-        }, 2000);
-        setTimeout(function () {
-            wall.fitWidth();
-            $(window).trigger("resize");
-        }, 3000);
     });
-
-
 
 })(window, document);
