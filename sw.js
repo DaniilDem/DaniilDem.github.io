277e084846917b38506d5221abb8481b1ebf050b
DaniilDem <dan9m@yandex.com>
2017-07-24 23:43:36 +0300

    update pwa cache

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["404.html","551b8847452868e48995b1934cdc8c3c"],["apple-icon-precomposed.png","d4eb82ed10bd3edbc59c3263fb078f35"],["dist/css/common.css","d4c73f97b6952820b8abc00ad5dd0661"],["dist/js/build.min.js","2efd354df5bd95f5be32ac6c71da78ff"],["favicon.png","e38801e4b7a424bd0f5a7ac8cfad7b98"],["favicon_big.png","d21883514e6a6603c9c95ed1c68621b5"],["gulpfile.babel.js","eb1a6f9d6a9261bcdeaf2b2d38140a3d"],["icon-512x512.png","71833f48bae5ec522c9ee65123cf5a4f"],["index.html","eb832adffed9d2212c90576d08fabe11"]];
+var precacheConfig = [["404.html","551b8847452868e48995b1934cdc8c3c"],["apple-icon-precomposed.png","d4eb82ed10bd3edbc59c3263fb078f35"],["dist/css/common.css","d4c73f97b6952820b8abc00ad5dd0661"],["dist/js/build.min.js","2efd354df5bd95f5be32ac6c71da78ff"],["favicon.png","e38801e4b7a424bd0f5a7ac8cfad7b98"],["favicon_big.png","d21883514e6a6603c9c95ed1c68621b5"],["gulpfile.babel.js","eb1a6f9d6a9261bcdeaf2b2d38140a3d"],["icon-512x512.png","71833f48bae5ec522c9ee65123cf5a4f"],["index.html","980f48db930f44671ec05fab2ba1f47d"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
